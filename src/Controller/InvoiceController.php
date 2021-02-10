db9e33c5213c274511f73d32ef3425771c0842df
pickforbass <56023752+pickforbass@users.noreply.github.com>
2021-02-10 17:04:00 +0100

    creating InvoiceController.php functions & vues

--- src/Controller/InvoiceController.php
+++ src/Controller/InvoiceController.php
@@ -9,7 +9,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class InvoiceController extends AbstractController
 {
     /**
-     * @Route("/invoice", name="invoice")
+     * @Route("/invoices", name="invoices")
      */
     public function index(): Response
     {
@@ -17,4 +17,27 @@ class InvoiceController extends AbstractController
             'controller_name' => 'InvoiceController',
         ]);
     }
+
+
+    /**
+     * @Route("/invoice/new", name="new-invoice")
+     */
+    public function newInvoice(): Response
+    {
+        return $this->render('invoice/new.html.twig', [
+            'controller_name' => 'Nouvelle facture',
+        ]);
+    }
+
+    /**
+     * @Route("/invoice/{id}", name="invoice")
+     */
+    public function invoice(): Response
+    {
+        return $this->render('invoice/index.html.twig', [
+            'controller_name' => 'InvoiceController',
+        ]);
+    }
+
+
 }
