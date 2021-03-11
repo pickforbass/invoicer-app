b0995ff04e5286804b072ab7e6b560fb4f27690b
pickforbass <56023752+pickforbass@users.noreply.github.com>
2021-03-11 14:40:30 +0100

    complete script ofr new rows in invoice/new

--- assets/scripts/invoice_scripts/invoice_new_row.js
+++ assets/scripts/invoice_scripts/invoice_new_row.js
@@ -4,105 +4,19 @@ const prototype = firstRow.outerHTML;
 const addRow = document.getElementById('add-row');
 let index = tableBody.rows.length;
 
+// Changing first row
 
-//Set new data-prototype by the first tr
-tableBody.setAttribute('data-prototype',prototype);
+firstRow.outerHTML = prototype.replaceAll('__name__', index);
+index++;
 
-// changing all __name__ into index number
 
-function putIndex(tr){
-
-    let content = tr.outerHTML.replaceAll('__name__', index.toString());
-    return tr.outerHTML = content;
-
+function insertRow(){
+    //insert index
+    let newRow = prototype.replaceAll('__name__', index);
+    // increment index
+    index++;
+    //insert new row
+    tableBody.innerHTML+=newRow;
 }
-
-putIndex(firstRow);
-
-
-
-
-//
-// index === 0 ? index = 1 : index = rows.length;
-//
-// function newRow(){
-//
-//     //Get Prototype
-//     prototype = tableBody.dataset.prototype;
-//
-//     // Changing div to tr
-//     prototype = prototype.replace('<div', "<tr class='row'");
-//     let strReverse = prototype.split('').reverse().join('');
-//     strReverse = strReverse.replace('>vid', '>rt');
-//     prototype = strReverse.split('').reverse().join('');
-//
-//     labelEraser(prototype);
-//
-//     prototype = divToTd(prototype);
-//
-//     prototype = insertMoreTd(prototype);
-//
-//     prototype = putIndex(prototype);
-//
-//     tableBody.innerHTML += prototype;
-//
-// }
-//
-//
-// function labelEraser(sentence) {
-//     let label = "<label";
-//     let endLabel = "</label>";
-//
-//     if (sentence.includes(label) || sentence.includes(endLabel)) {
-//         let start = sentence.indexOf(label);
-//         let end = sentence.indexOf(endLabel);
-//
-//         let toDelete = sentence.slice(start, end + endLabel.length);
-//
-//         let newSentence = sentence.replace(toDelete, '');
-//         prototype = newSentence;
-//         labelEraser(newSentence);
-//     }
-// }
-//
-// function divToTd(sentence){
-//     sentence = sentence.replaceAll('<div>','<td>');
-//     return sentence.replaceAll('</div>','</td>');
-// }
-//
-//
-// function insertMoreTd(sentence){
-//     let hourTd = document.createElement('td');
-//     let feeTd = document.createElement('td');
-//     let hourInput = document.createElement('input');
-//     let feeInput = document.createElement('input');
-//
-//     hourInput.setAttribute('name', 'hour___name__');
-//     hourInput.setAttribute('id', 'designation_hour___name__');
-//     feeInput.setAttribute('name', 'fee___name__');
-//     feeInput.setAttribute('id', 'designation_fee___name__');
-//
-//     hourTd.append(hourInput);
-//     feeTd.append(feeInput);
-//
-//
-//     let start = sentence.indexOf('</td>')+'</td>'.length;
-//     return sentence.slice(0, start)+hourTd.outerHTML+feeTd.outerHTML+sentence.slice(start, sentence.length);
-// }
-//
-// newRow();
-//
-// addRow.addEventListener("click", newRow);
-
-
-// // *************************************************************************************** //
-//
-//
-// let designationCollection = $("[id^='new_invoice_designation']");
-//
-// designationCollection.each(function() {
-//     let feeInput = $("[name^='fee_']");
-//     let option = $(this).find(':selected');
-//     console.log(option);
-// })
+addRow.addEventListener('click', insertRow);
 
